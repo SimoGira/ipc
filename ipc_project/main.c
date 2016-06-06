b3a6cd3e15a50937ee7e84ca3bbda90faeaf9fd7
SimoGira <s.girardi92@gmail.com>
2016-06-06 22:58:47 +0200

    Initial Commit

---
+++ ipc_project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ipc_project
+//
+//  Created by Simone Girardi on 06/06/16.
+//  Copyright © 2016 Simone Girardi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
