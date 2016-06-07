d47ec6cf8f0fd4297948f3751a9697aa3248d4dd
SimoGira <s.girardi92@gmail.com>
2016-06-08 01:29:03 +0200

    update

--- mylib.c
+++ mylib.c
@@ -16,6 +16,26 @@
 
 #include "mylib.h"
 
+int process_operation(int val1, int val2, char op){
+    switch (op) {
+        case '+':
+            return = val1 + val2;
+            break;
+        case '-':
+            return = val1 - val2;
+            break;
+        case '*':
+            return = val1 * val2;
+            break;
+        case '/':
+            return = val1 / val2;
+            break;
+        default:
+            return -1; // not valid operator
+            break;
+    }
+}
+
 //shared memory
 
 
