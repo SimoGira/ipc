5d4226c76c17e1ea53b6b09a866e5c57d2d426c2
TUO_USERNAME <adriano.tummi@hotmail.it>
2016-06-07 21:09:02 +0200

    update

--- ipc_calculator.c
+++ ipc_calculator.c
@@ -31,13 +31,18 @@
 
 struct sembuf P, V;
 
+
+#if (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) \
+    || defined(__FreeBSD__)  
+#else 
 union semun
 {
 	int val;                // value for SETVAL
 	struct semid_ds* buf;   // buffer for IPC_STAT, IPC_SET
 	unsigned short*  array; // array for GETALL, SETALL
 	struct seminfo*  __buf; // buffer for IPC_INFO
-};
+}; 
+#endif
 
 struct operation{
 	int id;
